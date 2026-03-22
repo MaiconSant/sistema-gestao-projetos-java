f8a4d320b617a62b3267ca09326675761ed520dd
MaiconSant <61200367+MaiconSant@users.noreply.github.com>
2026-03-22 11:45:45 -0300

    Criar Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,15 @@
+public class Usuario {
+    private String nome;
+    private String email;
+    private String cidade;
+
+    public Usuario(String nome, String email, String cidade) {
+        this.nome = nome;
+        this.email = email;
+        this.cidade = cidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
