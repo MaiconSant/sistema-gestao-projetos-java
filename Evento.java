ef8d15872d643e84b63fa4bc8f3495d88bf6de7b
MaiconSant <61200367+MaiconSant@users.noreply.github.com>
2026-03-22 11:46:08 -0300

    Criar Evento.java

---
+++ Evento.java
@@ -0,0 +1,29 @@
+import java.time.LocalDateTime;
+
+public class Evento {
+    private String nome;
+    private String endereco;
+    private String categoria;
+    private LocalDateTime horario;
+    private String descricao;
+
+    public Evento(String nome, String endereco, String categoria, LocalDateTime horario, String descricao) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.categoria = categoria;
+        this.horario = horario;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public LocalDateTime getHorario() {
+        return horario;
+    }
+
+    public String toString() {
+        return nome + " - " + categoria + " - " + horario;
+    }
+}
